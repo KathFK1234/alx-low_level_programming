02d2199ec035c549666d2d42e6b00db92e0a1fa6
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-12 08:45:45 -0700

    Scripts and programs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
