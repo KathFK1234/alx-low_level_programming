3ccb40da5804f901d2b925be960051676317bb91
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-13 09:52:30 -0700

    Program fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,12 @@ int main(void)
 	int k;
 	int m;
 
-	for (k = 0; k <= 9; k++)
+	for (m = 0; m <= 9; m++)
 	{
-		for (m = 0; m <= 9; m++)
+		for (k = 0; k <= 9; k++)
 		{
-			putchar(k);
 			putchar(m);
+			putchar(k);
 
 			if (m != '8' || k != '9')
 			{
