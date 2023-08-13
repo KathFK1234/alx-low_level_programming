82553b37fc6e693be75d36406fbf9398daf2ff54
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-13 11:02:14 -0700

    Program fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#include <stlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("less than 6 and not 0\n")
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
