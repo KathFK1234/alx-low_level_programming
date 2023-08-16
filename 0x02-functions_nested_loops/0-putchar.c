2c5e1f0dabfcb22847decfc4d8887a987144d04f
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-16 06:19:58 -0700

    Program attempt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,14 @@
-#include <>
+#include <stdio.h>
 
 /*
- * main - 
+ * main - Print _putchar, followed by a new line
  * Return: Always 0
  */
 
-int 
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+
+	return (0);
+}
