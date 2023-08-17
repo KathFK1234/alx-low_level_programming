2099e0610bca3f3e807307b45cb40116745632ac
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-16 18:01:17 -0700

    Forgot semi colon

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,7 +40,7 @@ void print_times_table(int n)
 					{
 						_putchar('0' + (product / 100));
 						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10))'
+						_putchar('0' + (product % 10));
 					}
 				}
 			}
