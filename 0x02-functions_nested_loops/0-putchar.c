eb24bb7c4bd11acda6f3f94651a91c166ebde423
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-16 17:19:49 -0700

    Program attempts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <unistd.h>
+#include "alxschool.h"
 
 /*
  * main - Print _putchar, followed by a new line
  * Return: Always 0
  */
 
-int _putchar(char _putchar)
+int main(void)
 {
-	return (write(0, &_putchar, 0));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
 }
