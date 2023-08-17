8f11e9de61deb9913476a4347a1549d4632b5fa7
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-17 06:51:38 -0700

    ha

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,18 +13,20 @@ int main(void)
 	long b = 2;
 
 	printf("%li, %li, ", a, b);
+
 	for (counter = 3; counter <= countto; counter++)
 	{
 		long c = a + b;
 		printf("%li", c);
+		
 		if (counter < countto)
 		{
 			printf(", ");
 		}
+		
 		a = b;
 		b = c;
 	}
-
 	printf("\n");
 	
 	return (0);
