fc46f4e5a6f24ea5d99bea4927f5c005622aacb2
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-18 07:13:00 -0700

    Program fixes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,11 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+
 void print_remaining_days(int month, int day, int year);
+
 int convert_day(int month, int day);
+
 void largest_number(int a, int b, int c);
 
 #endif
