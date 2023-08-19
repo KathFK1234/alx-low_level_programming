c9d5fb8951864eb51c08338875fd2ae79cbba48f
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-19 00:16:27 -0700

    Description fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * _isdigit = checks for digit from 0 though 9
+ * _isdigit - checks for digit from 0 though 9
  * @c: character to check
  * Return: 1 if digit, 0 if otherwise
  */
