4fe6dad9087d172629768d36f51812ef9b0f33cd
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-19 02:21:43 -0700

    P fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,33 +8,20 @@
 
 void print_number(int n)
 {
-	int b = 1;
-	unsigned int k = n;
-	unsigned int m = n;
-	unsigned int x = n;
-
 	if (n == 0)
 	{
 		_putchar('0');
 	}
-	else
+
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			x = n * -1;
-			_putchar('-');
-		}
-
-		while (b <= x)
-			b *= 10;
-		k = b / 10;
-
-		while (k >= 1)
-		{
-			m = x / k;
-			_putchar(m + '0');
-			x = (x - (k * m));
-			k /= 10;
-		}
+		_putchar('-');
+		n = -n;
 	}
+
+	if (n / 10 != 0)
+	{
+		_putchar(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
