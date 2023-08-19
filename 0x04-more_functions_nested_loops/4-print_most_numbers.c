ed6c7fde26bb6af9835c2645ecde0ffb1ac868c4
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-19 00:25:15 -0700

    Program fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main = Entry point
- * print_most_numbers = prints numbers from 0 to 9
+ * print_most_numbers - prints numbers from 0 to 9
  * Return: Always 0
  */
 
@@ -12,7 +11,7 @@ void print_most_numbers(void)
 
 	for (g = 0; g <= 9; g++)
 	{
-		if (g != '2' && g != '4')
+		if (g != 2 && g != 4)
 			_putchar(g);
 	}
 	_putchar('\n');
