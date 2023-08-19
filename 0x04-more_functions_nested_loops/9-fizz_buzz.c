80f492c891ac6e8a7e6caee903180f6646d5e8fd
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-19 00:59:11 -0700

    Program fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,22 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-			_putchar('Fizz');
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else if (x % 5 == 0)
 		{
-			_putchar('Buzz');
-		}
-		else if (x % 3 == 0 && x % 5 == 0)
-		{
-			_putchar('FizzBuzz');
+			printf("Buzz ");
 		}
 		else
 		{
-			_putchar('%d', x);
+			printf("%d ", x);
 		}
-
-		if (x != 100)
-			_putchar(' ');
 	}
-	_putchar('\n');
+	printf("Buzz\n")
 }
