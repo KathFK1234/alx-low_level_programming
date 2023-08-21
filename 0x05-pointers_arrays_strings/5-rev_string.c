4b5f0347253d244530835774b1b4557abc4d8aba
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-21 10:06:41 -0700

    Programs

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,3 +8,20 @@
 
 void rev_string(char *s)
 {
+	int c;
+	int k = 0;
+	char x[500];
+
+	for (c = 0; (*(s + c)) != '0'; c++)
+	{
+		x[c] = *(s + c);
+	}
+
+	c--;
+
+	for (c = 0; c >= 0; c--)
+	{
+		*(s + k) = n[c];
+		k++;
+	}
+}
