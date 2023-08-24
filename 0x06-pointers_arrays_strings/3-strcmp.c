a46792322bf998efa7ed5c56a098bb22b5f24739
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-24 03:31:41 -0700

    P fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int x;
 
-	for (x = 0; (s1[x]x - s2[x]) == 0 && s1[x] != '\0'; x++)
+	for (x = 0; (s1[x] - s2[x]) == 0 && s1[x] != '\0'; x++)
 	{}
 
 	return (s1[x] - s2[x]);
