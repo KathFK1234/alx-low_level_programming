bf64092ba66222b04cfe72d003db6e12dbbedd28
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-08-24 04:00:40 -0700

    P fix maybe

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,3 +8,17 @@
 
 char *cap_string(char *s)
 {
+	int x, alpha;
+	int sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (x = 0; sep[x] != '\0'; x++)
+	{
+		for (alpha = 0; s[alpha] != '\0'; alpha++)
+		{
+			if (s[alpha] >= 'a' && s[alpha] <= 'z')
+			{
+				s[alpha] = s[alpha] - 32;
+			}
+		}
+	return (s);
+}
