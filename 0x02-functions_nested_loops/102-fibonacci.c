363382eb2c793bfbd94b67ee54375b945d819d9d
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-02 07:42:32 -0700

    Program fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 	int countto = 50;
 	long a = 1;
 	long b = 2;
+	long c = a + b;
 
 	printf("%li, %li, ", a, b);
 	for (counter = 3; counter <= countto; counter++)
 	{
-		long c = a + b;
 		printf("%li", c);
 		if (counter < countto)
 		{
