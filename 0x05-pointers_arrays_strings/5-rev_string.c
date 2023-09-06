4c246d0533f5646365d2c8ce5224aa5d19f899ef
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-06 07:02:56 -0700

    Program fixes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,11 +12,11 @@ void rev_string(char *s)
 
 	for (c = 0; *(s + c) != '\0'; c++)
 	{
-		_putchar(*(s + c));
+		_putchar(s[c]);
 	}
 
 	for (c = c - 1; c >= 0; c--)
 	{
-		_putchar(*(s + c));
+		_putchar(s[c]);
 	}
 }
