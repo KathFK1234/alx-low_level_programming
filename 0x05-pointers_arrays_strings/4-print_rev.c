4f7cb0493fbc335781a8e93b2f3957a5425e2e5a
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-06 06:08:18 -0700

    Ha

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,21 @@
 void print_rev(char *s)
 {
 	int c;
+	char i;
 
 	for (c = 0; *(s + c) != '\0'; c--)
 	{
 		s--;
 		_putchar(*(s + c));
+	}
+
+	while (i >= 'a' && i <= 'z')
+	{
+		while (i >= 'A' && i <= 'Z')
+		{
+			_putchar(*(s + i);
+			s--;
+		}
 	}
 	_putchar('\n');
 }
