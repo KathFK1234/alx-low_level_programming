c55028936819f22ca0347924668ec90e51b829c5
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-09 11:46:35 -0700

    Programs

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: first string
+ * @needle: second string
+ * Return: Pointer to beginning of located substring, NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int x, y;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		for (y = 0; needle[y] != '\0'; y++)
+		{
+			if (haystack[x] == needle[y])
+				return (haystack + x);
+		}
+	}
+	return ('\0');
+}
