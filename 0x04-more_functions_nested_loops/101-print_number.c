af8ecd4568e24ac465dfde3175e57916c1faaaa5
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-10 08:41:54 -0700

    P fix ig

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	if (n == 0)
+	if (n == '0')
 	{
 		_putchar(n);
 	}
@@ -23,5 +23,6 @@ void print_number(int n)
 	{
 		print_number(n / 10);
 	}
+	_putchar('0' + (n / 10));
 	_putchar('0' + (n % 10));
 }
