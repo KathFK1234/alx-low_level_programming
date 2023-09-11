54ff7eef9c2281d05cae91f48b0ae5a7e79b2a9b
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-11 10:23:03 -0700

    Program fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,3 +19,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ndog == NULL)
 		return (NULL);
 
+	for (bname = 0; name[bname]; bname++)
+		;
+
+	for (bowner = 0; owner[bowner]; bowner++)
+		;
+
+	ndog->name = malloc(bname + 1);
+	ndog->owner = malloc(bowner + 1);
+
+	if (!(ndog->name) || !(ndog->owner))
+	{
+		free(ndog->name);
+		free(ndog->owner);
+		free(ndog);
+		return (NULL);
+	}
+
+	for (x = 0; x < bname; x++)
+		ndog->name[x] = name[x];
+
+	ndog->name[x] = '\0';
+	ndog->age = age;
+
+	for (x = 0; x < bowner; x++)
+		ndog->owner[x] = owner[x];
+
+	ndog->owner[x] = '\0';
+	return (ndog);
+}
