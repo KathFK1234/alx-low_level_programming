3c2363a56f64e4947e26bf73e936b1903fbc2b62
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-11 05:52:16 -0700

    Fix attempt

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,15 +12,18 @@
 int main(int argc, char *argv[])
 {
 	int x = 1;
-	int y = argc - x;
-	int a = x + y;
+	int y = 2;
+	int a;
 
-	for (; x < argc; x++)
+	if (x < argc)
 	{
 		for (; y < argc; y++)
 		{
 			if (argv[x] != argv[argc] && argv[y] != argv[argc])
+			{
+				argv[a] = argv[x] + argv[y];
 				printf("%s\n", argv[a]);
+			}
 			else if (argv[x] == argv[argc] && argv[y] == argv[argc])
 				printf("0\n");
 			else
