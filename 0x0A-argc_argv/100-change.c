235f3fddb8b2a0b57a3fb447bb81ff850fe93f66
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-11 10:59:16 -0700

    Program attempt

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,41 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- *
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv)
+{
+	int x, y, z, a, b, c;
+	unsigned int cents[5] = {1, 2, 5, 10, 25};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
+	cents = atoi(argv[1]);
+	if (argc == 2)
+	{
+		for (x = 0; argv[x] != '\0'; x++)
+		{
+			y = atoi(argv[x]);
+			z = atoi(cents[x]);
+			a = y / z;
+			b = y % z;
+			c = a + b;
+		}
+		printf("%d\n", c);
+	}
+	return (0);
+}
