e8de3b60237510964263cf7d7ce422da593bcc91
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-11 09:21:54 -0700

    Main header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - defines dog macro
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - definition for dog struct
+ */
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
