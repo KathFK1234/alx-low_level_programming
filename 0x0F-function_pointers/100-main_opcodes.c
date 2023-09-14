fb686e1cb568938c001fcf509999f5f9ee0dfeb6
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-14 06:40:23 -0700

    Program

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,42 @@
-later
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	x = atoi(argv[1]);
+
+	if (x < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	p = (char *)main;
+
+	for (y = 0; y < x; y++)
+	{
+		if (y == x - 1)
+		{
+			printf("%02hhx\n", p[y]);
+			break;
+		}
+		printf("%02hhx\n", p[y]);
+	}
+	return (0);
+}
