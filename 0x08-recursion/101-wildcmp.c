02a922ae3e0668b1256962dbf758face508807cd
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-18 03:37:10 -0700

    Program fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,13 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	int x = 0;
-	int y = 0;
-
-	if ((s1[x] - s2[y]) == 0 && s1[x] != '\0')
-	{
-		x++;
-		y++;
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	}
-	else if (x == '*' || y == '*')
-	{
-		if (s1[x] == s2[x] && s1[x] != '\0')
-			return (1);
-	}
 
+	if (*s1 == *s2 || *s2 == '*')
+		return wildcmp(s1 + 1, s2 + 1);
+
+	if (*s2 == '*')
+		return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
 	return (0);
-	wildcmp(s1, s2);
 }
