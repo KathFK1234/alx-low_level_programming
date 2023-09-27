dde1902a7b2c755e8f777660663bd0261f0784da
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-27 11:03:08 -0700

    Fix attempt

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,11 +20,13 @@ int wildcmp(char *s1, char *s2)
 		y++;
 		return (1);
 	}
-	else if (s1[x] == "*" || s2[y] == "*")
+	else if (*s1 == "*" || *s2 == "*")
 	{
 		if (s1[x] == s2[x] && s1[x] != '\0')
 			return (1);
 	}
 	else
 		return (0);
+
+	return (1);
 }
