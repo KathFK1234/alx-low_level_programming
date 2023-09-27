488f128500192ca07272c391692745e26d451d4e
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2023-09-27 10:34:02 -0700

    Fix attempt

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,13 +17,16 @@ int is_palindrome(char *s)
 
 	if (*s != '\0')
 	{
-		while (x = 0)
+		while (x == 0)
 		{
 			if (s[x] != '\0')
+			{
 				if (x++ == --x)
 					return (1);
 				else
 					return (0);
+			}
 		}
 	}
+	return (1);
 }
