9f2c6114f791d8288cc79cb962b002785d05532b
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-07-02 06:21:42 -0700

    Correction attempt

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,14 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if (x % 15 == 0)
 			printf("FizzBuzz ");
 		else if (x % 3 == 0)
 			printf("Fizz ");
-		else if (x % 5 == 0)
-		{
-			if (x == 100)
-				printf("Buzz");
+		else if (x % 5 == 0 && x == 100)
+			printf("Buzz");
+		else if (x % 5 == 0 && x != 100)
 			printf("Buzz ");
-		}
 		else
 			printf("%d ", x);
 	}
